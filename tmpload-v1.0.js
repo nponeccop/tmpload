c4b07bf2f020c0a4d5c8f2f0d1834f9a9b5ac2f5
nponeccop <andy.melnikov@gmail.com>
2011-08-13 15:22:57 +0300

    Edited tmpload-v1.0.js via GitHub

--- tmpload-v1.0.js
+++ tmpload-v1.0.js
@@ -27,18 +27,32 @@ github.com/markdalgleish/tmpload/blob/master/MIT-LICENSE.txt
             else if (typeof obj === "string") {
                 if (typeof templates[obj] === "string") {
                     //The template hasn't been loaded yet
-                    return $.Deferred(function(dfd) {
-                        $.get(templates[obj]).success(function(d) {
-                            templates[obj] = $.template(d);
-                            dfd.resolve(templates[obj]);
-                        }).error(function(d) {
-                            dfd.reject(d);
-                        });
-                    }).promise();
-                } else {
+
+                    			// pipe() decorates the Deferred object returned by get(), so get().success()
+					// passes precompiled template instead of raw html. 
+					// get.fail() works undecorated as before.
+					// The decorated Deferred object stores the precompiled template internally, so the 
+					// compilation is called only once just after the HTTP GET.
+					// 
+					// Contrary to what the name suggests, Deferred object is not delaying the computation
+					// - the computation (HTTP GET and template compilation) starts immediately and only 
+					// synchronization is delayed.
+					// 
+					// So we cannot use Deferred to avoid executing get() twice. We need either an 
+					// implementation of abstract memoized thunk or to simulate the 'already evaluated' 
+					// check of the memoized thunk using typeof dfd === 'string' condition as the flag
+
+					templates[obj] = $.get(templates[obj]).pipe(function (tmplStr)
+					{
+						// template() requires two arguments - the first argument can be null but it  
+						// not optional in the documentation. However
+						// but we pass template name here to support nested templates feature of 
+						// jQuery templates
+						return $.template(obj, tmplStr)
+					})
+                }
                     //The template has already been cached
                     return templates[obj];
-                }
             }
         //Declaring a single template
         } else {
